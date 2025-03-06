461bfdaf0e397072d48a3708ec9e8c0534c46123
sometimes-maybegood <maratakhun@gmail.com>
2025-03-06 16:37:45 +0300

    2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask
+
 from data import db_session
+from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -7,7 +9,14 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/blogs.db")
-    app.run()
+    # app.run()
+    user = User()
+    user.name = "Пользователь 1"
+    user.about = "биография пользователя 1"
+    user.email = "email@email.ru"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
 
 
 if __name__ == '__main__':
